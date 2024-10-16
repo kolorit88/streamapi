a896beae448363a286b64f07f667d791958ef26f
DELL <koloritlol@mail.ru>
2024-10-16 13:09:22 +0700

    com2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -27,8 +27,18 @@ public class Main {
 
         List<Book>favBooks = allBooks.stream().distinct().toList();
 
-
         System.out.println("Visitors favorite books:" + favBooks);
         System.out.println("Visitors count:" + favBooks.size());
+
+        System.out.println("\n\n3)");
+
+        allBooks = favBooks.stream().sorted(Comparator.comparing(Book::getPublishingYear)).collect(Collectors.toList());
+        System.out.println("Books sorted by year:" + allBooks);
+
+        System.out.println("\n\n4)");
+        System.out.println("Jane Austen :" + allBooks.stream().filter(book -> Objects.equals(book.getAuthor(), "Jane Austen")).count());
+
+        System.out.println("\n\n5)");
+        System.out.println(Arrays.stream(visitors).max((h1, h2) -> Integer.compare(h1.getFavoriteBooks().size(), h2.getFavoriteBooks().size())).get().getFavoriteBooks().size());
     }
 }
